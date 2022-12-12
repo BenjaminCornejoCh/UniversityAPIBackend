397419072ac70e794fd219d7c9b2d4d1cb503d73
Benjamin <benjamin.cornejo1@unmsm.edu.pe>
2022-12-11 22:37:38 -0500

    University API v1

---
+++ myBackend/DataAccess/UniversityDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UniversityApiBackend.Models.DataModels;
+
+namespace UniversityApiBackend.DataAccess
+{
+    public class UniversityDBContext: DbContext
+    {
+        public UniversityDBContext(DbContextOptions<UniversityDBContext> options): base(options)
+        {
+
+        }
+        // TODO: Add DBSets (Tables of our Ddata base)
+        public DbSet<User>? Users { get; set; }
+        public DbSet<Course>? Courses { get; set; }
+        public DbSet<Chapter> Chapters { get; set; }
+        public DbSet<Category>? categories { get; set; }
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ myBackend/obj/Debug/net6.0/xda15t0b.sql
@@ -0,0 +1,67 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+CREATE TABLE [Users] (
+    [Id] int NOT NULL IDENTITY,
+    [Name] nvarchar(50) NOT NULL,
+    [LastName] nvarchar(100) NOT NULL,
+    [Email] nvarchar(max) NOT NULL,
+    [Password] nvarchar(max) NOT NULL,
+    [CreatedBy] nvarchar(max) NOT NULL,
+    [CreatedAt] datetime2 NOT NULL,
+    [UpdatedBy] nvarchar(max) NOT NULL,
+    [UpdatedAt] datetime2 NULL,
+    [DeletedBy] nvarchar(max) NOT NULL,
+    [DeletedAt] datetime2 NULL,
+    [IsDeleted] bit NOT NULL,
+    CONSTRAINT [PK_Users] PRIMARY KEY ([Id])
+);
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20221210060224_Create Users table', N'7.0.0');
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+CREATE TABLE [Courses] (
+    [Id] int NOT NULL IDENTITY,
+    [Name] nvarchar(100) NOT NULL,
+    [ShortDescription] nvarchar(280) NOT NULL,
+    [LongDescription] nvarchar(max) NOT NULL,
+    [TargetAudience] nvarchar(max) NOT NULL,
+    [Objectives] nvarchar(max) NOT NULL,
+    [Requirements] nvarchar(max) NOT NULL,
+    [Level] int NOT NULL,
+    [CreatedBy] nvarchar(max) NOT NULL,
+    [CreatedAt] datetime2 NOT NULL,
+    [UpdatedBy] nvarchar(max) NOT NULL,
+    [UpdatedAt] datetime2 NULL,
+    [DeletedBy] nvarchar(max) NOT NULL,
+    [DeletedAt] datetime2 NULL,
+    [IsDeleted] bit NOT NULL,
+    CONSTRAINT [PK_Courses] PRIMARY KEY ([Id])
+);
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20221210064220_Create Courses table', N'7.0.0');
+GO
+
+COMMIT;
+GO
+
